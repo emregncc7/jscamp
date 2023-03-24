ed53fd9a4ffa1cc494f28a462ad3f9dabfdef51a
Emre Genç <emregenc696@gmail.com>
2023-03-24 10:04:39 +0300

    2403

--- src/components/userComponent.js
+++ src/components/userComponent.js
@@ -3,39 +3,18 @@ import Customer from "../models/customer.js"
 import User from "../models/user.js"
 import UserService from "../services/userService.js"
 
-console.log("User component yüklendi")
+// let dolarDun = 25;
+// let dolarBugun = 30;
+// if (dolarDun<dolarBugun) {
+//     console.log("Dolar Arttı")
+// }
+// else{
+//     console.log("Dolar Düştü")
+// }
 
-let logger1 = new MongoLogger()
-let userService = new UserService(logger1)
-
-let user1 = new User(1,"Engin","Demiroğ","Ankara")
-let user2 = new User(2,"Baran","Gökçekli","Muğla")
-userService.add(user1)
-userService.add(user2)
-
-//console.log(userService.list())
-//console.log(userService.getById(2))
+let konutKredis = ["Tasıt kredisi","konut kredisi","ihtiyac kredisi","Sup kredisi"]
 
 
-
-
-let customer = {id:1, firstName:"Engin"}
-
-//prototyping
-customer.lastName = "Demiroğ"
-
-console.log(customer.lastName)
-
-console.log("--------------------------")
-userService.load()
-
-
-let customerToAdd = new Customer(1,"Seda","Yılmaz","Ankara","fdgdfg");
-customerToAdd.type = "customer"
-
-userService.add(customerToAdd)
-console.log(userService.customers)
-console.log(userService.employees)
-console.log(userService.errors)
-console.log(userService.getCustomersSorted())
-//22.00 Dersteyiz
+for(let i =0; i<konutKredis.length;i++ ){
+console.log(konutKredis[i])
+}
